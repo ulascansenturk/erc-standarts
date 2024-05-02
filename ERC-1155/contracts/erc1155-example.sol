f91639a4cddf6ee35f2cee70b29bca34186e3795
ulascansenturk <ulascansenturk@protonmail.com>
2024-05-02 23:40:29 +0300

    add allowlist, and allowlist mint

--- ERC-1155/contracts/erc1155-example.sol
+++ ERC-1155/contracts/erc1155-example.sol
@@ -9,18 +9,45 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract ERC1155Example is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
-    uint256 public nftPrice = 0.01 ether;
-    uint256 public maxSupply = 8888;
+uint256 public publicPrice = 0.02 ether;
+    uint256 public allowListPrice = 0.01 ether;
+    uint256 public maxSupply = 20;
+    uint public maxPerWallet = 3;
 
-    constructor(address initialOwner)
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = true;
+
+    mapping(address => bool) allowList;
+    mapping(address => uint256) purchasesPerWallet;
+
+    constructor(
+        address[] memory _payees,
+        uint256[] memory _shares
+    )
         ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
-        Ownable(initialOwner)
+        PaymentSplitter(_payees, _shares)
     {}
+
+
+    // Create an edit function that will edit the mint windows
+    function editMintWindows(
+        bool _publicMintOpen,
+        bool _allowListMintOpen
+    ) external onlyOwner {
+        publicMintOpen= _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
+    }
+
+    // Create a function to set the allowlist
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i = 0; i< addresses.length; i++){
+            allowList[addresses[i]] = true;
+        }
+    }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
-
 
     function pause() public onlyOwner {
         _pause();
@@ -30,25 +57,41 @@ contract ERC1155Example is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
         _unpause();
     }
 
-
-    function uri(uint256 _id) public view override virtual returns (string memory) {
-        require(exists(_id), "URI: nonexistent token");
-        return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
+    function allowListMint(uint256 id, uint256 amount) public payable {
+        require(allowListMintOpen, "Allow List mint is closed ");
+        require(allowList[msg.sender], "You are not on the allowlist");
+        require(msg.value == allowListPrice * amount);
+        mint(id, amount);
     }
 
-    function withdraw(address _addr) external onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(_addr).transfer(balance);
-    }
-
-
-    function mint( uint256 id, uint256 amount)
+    // add supply tracking
+    function publicMint(uint256 id, uint256 amount)
         public
         payable
     {
-        require(msg.value == nftPrice * amount, "NOT ENOUGH ETHERS SENT");
-        require(totalSupply(id) + amount <= maxSupply);
+        require(publicMintOpen, "Public mint closed");
+        require(id < 2, "Sorry looks like you are trying to mint the wrong NFT");
+        require(msg.value == publicPrice * amount, "WRONG! Not enough money sent");
+        mint(id, amount);
+    }
+
+    function mint(uint256 id, uint256 amount) internal {
+        require(purchasesPerWallet[msg.sender] + amount <=maxPerWallet, "Wallet limit reached");
+        require(id < 2, "Sorry looks like you are trying to mint the wrong NFT");
+        require(totalSupply(id) + amount  <= maxSupply, "Sorry we have minted out!");
         _mint(msg.sender, id, amount, "");
+        purchasesPerWallet[msg.sender] += amount;
+    }
+
+    function withdraw(address _addr) external onlyOwner {
+        uint256 balance = address(this).balance; // get the balance of the smart contract
+        payable(_addr).transfer(balance);
+    }
+
+    function uri(uint256 _id) public view virtual override returns (string memory) {
+        require(exists(_id), "URI: nonexistent token");
+        
+        return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
@@ -58,13 +101,12 @@ contract ERC1155Example is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
         _mintBatch(to, ids, amounts, data);
     }
 
-
-    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
-        override(ERC1155, ERC1155Pausable, ERC1155Supply)
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
     {
-        super._update(from, to, ids, values);
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
-
 
 }
